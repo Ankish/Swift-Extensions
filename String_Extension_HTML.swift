6b463eb7bac10a1cd9fdf01fcfa834bffe650458
Ankish <ankish5@gmail.com>
2015-12-10 10:48:36 +0530

    Create String_Extension_HTML.swift

---
+++ String_Extension_HTML.swift
@@ -0,0 +1,44 @@
+extension String {
+	/// Returns the String with all special HTML characters encoded.
+	public var stringByEncodingHTML: String {
+		var ret = ""
+		var g = self.unicodeScalars.generate()
+		while let c = g.next() {
+			if c < UnicodeScalar(0x0009) {
+				ret.appendContentsOf("&#x");
+				ret.append(UnicodeScalar(0x0030 + UInt32(c)));
+				ret.appendContentsOf(";");
+			} else if c == UnicodeScalar(0x0022) {
+				ret.appendContentsOf("&quot;")
+			} else if c == UnicodeScalar(0x0026) {
+				ret.appendContentsOf("&amp;")
+			} else if c == UnicodeScalar(0x0027) {
+				ret.appendContentsOf("&#39;")
+			} else if c == UnicodeScalar(0x003C) {
+				ret.appendContentsOf("&lt;")
+			} else if c == UnicodeScalar(0x003E) {
+				ret.appendContentsOf("&gt;")
+			} else if c > UnicodeScalar(126) {
+				ret.appendContentsOf("&#\(UInt32(c));")
+			} else {
+				ret.append(c)
+			}
+		}
+		return ret
+	}
+	
+	/// Returns the String with all special URL characters encoded.
+	public var stringByEncodingURL: String {
+		var ret = ""
+		var g = self.utf8.generate()
+		while let c = g.next() {
+			if c.shouldURLEncode {
+				ret.append(UnicodeScalar(37))
+				ret.appendContentsOf(c.hexString)
+			} else {
+				ret.append(UnicodeScalar(c))
+			}
+		}
+		return ret
+	}
+}
